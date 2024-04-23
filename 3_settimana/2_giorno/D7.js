a09798cc808ad3530b6438b32a10c957b019f4e5
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-04-23 17:52:01 +0200

    Correzione w5d2, e modifica file con nuovo formattatore Prettier

--- 3_settimana/2_giorno/D7.js
+++ 3_settimana/2_giorno/D7.js
@@ -4,11 +4,11 @@
 */
 
 const concatString = (str1, str2) => {
-  let str3 = str1.slice(0,2).concat(str2.slice(-3));
+  let str3 = str1.slice(0, 2).concat(str2.slice(-3));
   return str3.toUpperCase();
-}
+};
 
-console.log(concatString('calzino', 'ventola'));
+console.log(concatString("calzino", "ventola"));
 /*function newString(a, b) {
   let aNew = a.substr(0,2) + a.substr(-3,3 );
   let bNew = b.substr(0,2) + b.substr(-3,3 );
@@ -25,7 +25,7 @@ console.log(newString(str1, str2));*/
 function arrayDieci() {
   let array10 = [];
   for (let i = 0; i < 10; i++) {
-    array10.push(Math.floor(Math.random()*101));
+    array10.push(Math.floor(Math.random() * 101));
   }
   return array10;
 }
@@ -45,9 +45,9 @@ console.log(random10());*/
 */
 
 const evenArray = (array) => {
-  return array.filter((elemento) => elemento%2 === 0)
-}
-console.log(evenArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+  return array.filter((elemento) => elemento % 2 === 0);
+};
+console.log(evenArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 /*let array03 = arrayDieci();
 console.log(array03);
@@ -64,19 +64,19 @@ console.log(array03.filter(numeriPari));*/
 
 const sommaArray = (array) => {
   let somma = 0;
-  array.forEach((elemento) => somma += elemento)
+  array.forEach((elemento) => (somma += elemento));
   return somma;
-}
-console.log(sommaArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+};
+console.log(sommaArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 /* ESERCIZIO 5 (reduce)
   Scrivi una funzione per sommare i numeri contenuti in un array
 */
 
 const accorciaArray = (array) => {
-  return array.reduce((elemento, elemento2) => elemento += elemento2, 0)
-}
-console.log(accorciaArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+  return array.reduce((elemento, elemento2) => (elemento += elemento2), 0);
+};
+console.log(accorciaArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 /*let array05 = arrayDieci();
 console.log(5);
@@ -91,9 +91,9 @@ function sommaTutto(primo, altri) {
 */
 
 const aggiungiValore = (array, num) => {
-  return array.map((elemento) => elemento + num)
-}
-console.log(aggiungiValore([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7))
+  return array.map((elemento) => elemento + num);
+};
+console.log(aggiungiValore([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
 
 /*let array06 = arrayDieci();
 console.log(6);
@@ -111,8 +111,8 @@ console.log(incrementaArray(array06, 10));*/
 
 const lunghezzaStr = (array) => {
   return array.map((elemento) => elemento.length);
-}
-console.log(lunghezzaStr(['EPICODE', 'is', 'great']));
+};
+console.log(lunghezzaStr(["EPICODE", "is", "great"]));
 
 /*let array07 = ['tank', 'flu', 'movie', 'alcohol', 'sympathetic', 'wisecrack']
 console.log(7);
@@ -125,147 +125,147 @@ console.log(array07.map(lungStr));*/
   Scrivi una funzione per creare un array contenente tutti i valori DISPARI da 1 a 99.
 */
 
- function numeriDispari() {
+function numeriDispari() {
   let arrDispari = [];
   for (let i = 0; i < 100; i++) {
-    if (i%2 !== 0) {
-      arrDispari.push(i)
+    if (i % 2 !== 0) {
+      arrDispari.push(i);
     }
   }
   return arrDispari;
- }
+}
 console.log(numeriDispari());
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
 const movies = [
   {
-    Title: 'The Lord of the Rings: The Fellowship of the Ring',
-    Year: '2001',
-    imdbID: 'tt0120737',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    imdbID: "tt0120737",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lord of the Rings: The Return of the King',
-    Year: '2003',
-    imdbID: 'tt0167260',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    imdbID: "tt0167260",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lord of the Rings: The Two Towers',
-    Year: '2002',
-    imdbID: 'tt0167261',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Two Towers",
+    Year: "2002",
+    imdbID: "tt0167261",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of War',
-    Year: '2005',
-    imdbID: 'tt0399295',
-    Type: 'movie',
+    Title: "Lord of War",
+    Year: "2005",
+    imdbID: "tt0399295",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
   },
   {
-    Title: 'Lords of Dogtown',
-    Year: '2005',
-    imdbID: 'tt0355702',
-    Type: 'movie',
+    Title: "Lords of Dogtown",
+    Year: "2005",
+    imdbID: "tt0355702",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lord of the Rings',
-    Year: '1978',
-    imdbID: 'tt0077869',
-    Type: 'movie',
+    Title: "The Lord of the Rings",
+    Year: "1978",
+    imdbID: "tt0077869",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of the Flies',
-    Year: '1990',
-    imdbID: 'tt0100054',
-    Type: 'movie',
+    Title: "Lord of the Flies",
+    Year: "1990",
+    imdbID: "tt0100054",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lords of Salem',
-    Year: '2012',
-    imdbID: 'tt1731697',
-    Type: 'movie',
+    Title: "The Lords of Salem",
+    Year: "2012",
+    imdbID: "tt1731697",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
   },
   {
-    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
-    Year: '1984',
-    imdbID: 'tt0087365',
-    Type: 'movie',
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+    Year: "1984",
+    imdbID: "tt0087365",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of the Flies',
-    Year: '1963',
-    imdbID: 'tt0057261',
-    Type: 'movie',
+    Title: "Lord of the Flies",
+    Year: "1963",
+    imdbID: "tt0057261",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
   },
   {
-    Title: 'The Avengers',
-    Year: '2012',
-    imdbID: 'tt0848228',
-    Type: 'movie',
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Infinity War',
-    Year: '2018',
-    imdbID: 'tt4154756',
-    Type: 'movie',
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Age of Ultron',
-    Year: '2015',
-    imdbID: 'tt2395427',
-    Type: 'movie',
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Endgame',
-    Year: '2019',
-    imdbID: 'tt4154796',
-    Type: 'movie',
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
-]
+];
 
 /* ESERCIZIO 9 (forEach)
   Scrivi una funzione per trovare il film più vecchio nell'array fornito.
 */
 
 const filmVecchio = (array) => {
-  let result = {Year: 2100};
+  let result = { Year: 2100 };
   array.forEach((movie) => {
-    let currentYear = parseInt(movie.Year)
+    let currentYear = parseInt(movie.Year);
     if (currentYear < result.Year) {
-      result = movie
+      result = movie;
     }
-  })
+  });
   return result;
-}
+};
 console.log(filmVecchio(movies));
 
 /* ESERCIZIO 10
@@ -283,7 +283,7 @@ console.log(numeroFilm(movies));
 
 const onlyTitles = (array) => {
   return array.map((elemento) => elemento.Title);
-}
+};
 console.log(onlyTitles(movies));
 
 /* ESERCIZIO 12 (filter)
@@ -292,7 +292,7 @@ console.log(onlyTitles(movies));
 
 const nuoviFilm = (array) => {
   return array.filter((movie) => parseInt(movie.Year) > 1999);
-}
+};
 console.log(nuoviFilm(movies));
 
 /* ESERCIZIO 13 (reduce)
@@ -300,25 +300,25 @@ console.log(nuoviFilm(movies));
 */
 
 const sommaAnni = (array) => {
-  return array.reduce((acc, cur) => acc + parseInt(cur.Year), 0)
-}
-console.log(sommaAnni(movies))
+  return array.reduce((acc, cur) => acc + parseInt(cur.Year), 0);
+};
+console.log(sommaAnni(movies));
 
 /* ESERCIZIO 14 (find)
   Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
 */
 
-let codiceIMDB = 'tt0057261'
+let codiceIMDB = "tt0057261";
 const trovaFilm = (array, codice) => {
-  return array.find((movie) => movie.imdbID === codice)
-}
-console.log(trovaFilm(movies, codiceIMDB))
+  return array.find((movie) => movie.imdbID === codice);
+};
+console.log(trovaFilm(movies, codiceIMDB));
 
 /* ESERCIZIO 15 (findIndex)
   Scrivi una funzione per ottenere dall'array fornito l'indice del primo film uscito nell'anno fornito come parametro.
 */
 
 const posizioneFilm = (array, codice) => {
-  return array.findIndex((movie) => movie.imdbID === codice)
-}
-console.log(posizioneFilm(movies, codiceIMDB))
+  return array.findIndex((movie) => movie.imdbID === codice);
+};
+console.log(posizioneFilm(movies, codiceIMDB));
