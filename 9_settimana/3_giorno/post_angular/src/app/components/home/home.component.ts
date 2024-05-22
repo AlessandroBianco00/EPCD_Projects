2b1f55405a4fb15a146878498bb24d2d2a96f97d
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-05-22 19:02:43 +0200

    Completato esercizio w9d3. Aggiunta ngIf in html, ngfor per post consigliati.. Risoluzione shuffle con estrazione

--- 9_settimana/3_giorno/post_angular/src/app/components/home/home.component.ts
+++ 9_settimana/3_giorno/post_angular/src/app/components/home/home.component.ts
@@ -12,9 +12,34 @@ export class HomeComponent {
   urlJson:string = '../../../assets/db.json'
 
   arrayPost:iPost[] = []
+  arrayPool:number[] =[]
+  arrayEstrazione:iPost[] =[]
 
   ngOnInit() {
     this.getPosts()
+  }
+
+  shuffleArray(array:any[]) {
+     for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  getRandomPosts():void {
+    let postPool = this.arrayPost.length
+    let numeroPostEstratti:number = 4
+    for (let i=0 ; i < postPool; i++ ) {
+      this.arrayPool.push(i)
+    }
+
+    this.shuffleArray(this.arrayPool)
+
+    for (let j=0; j < numeroPostEstratti ; j++) {
+      let postEstratto = this.arrayPool.pop()
+      if(postEstratto) this.arrayEstrazione.push(this.arrayPost[postEstratto])
+    }
+    console.log(this.arrayEstrazione)
   }
 
   async getPosts():Promise<void>   {
@@ -22,6 +47,20 @@ export class HomeComponent {
     let object = <iPostVessel> await response.json()
 
     this.arrayPost = object.posts
-    console.log(this.arrayPost)
+
+    this.getRandomPosts()
   }
 }
+
+/* SHUFFLE FISHER-YATES FUNZIONA CORRETTAMENTE
+function shuffleArray(array:any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}*/
