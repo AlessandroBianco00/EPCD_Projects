05eb9cc2763fe36be6032fd4ab05b9ece85a6336
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-05-22 14:25:27 +0200

    Creazione struttura angular esercizio w9d3

---
+++ 9_settimana/3_giorno/post_angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
