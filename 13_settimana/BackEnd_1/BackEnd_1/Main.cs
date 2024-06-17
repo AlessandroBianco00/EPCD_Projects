792fc05786df417c8e81d9d7b7d6be209a5033e0
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-17 16:40:08 +0200

    Creato il main e inseriti variabili classi

---
+++ 13_settimana/BackEnd_1/BackEnd_1/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_1
+{
+    internal class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Atleta Lebron = new Atleta();
+            Lebron.FirstName = "Lebron";
+            Lebron.LastName = "James";
+            Lebron.Sport = "Basketball";
+            Lebron.Age = 40;
+
+            Lebron.BriefIntroduction();
+
+            Dipendente Dipendente1 = new Dipendente();
+            Dipendente1.FirstName = "Mario";
+            Dipendente1.LastName = "Rossi";
+            Dipendente1.Company = "Tetrapack";
+            Dipendente1.Job = "Junior Developer";
+            Dipendente1.Age = 40;
+
+            Console.WriteLine(Dipendente1.FirstName, Dipendente1.LastName, Dipendente1.Job);
+
+            Animale OrsoPolare = new Animale();
+            OrsoPolare.Species = "Polar bear";
+            OrsoPolare.Name = "Pippo";
+            OrsoPolare.Habitat = "South Pole";
+
+            OrsoPolare.HabitatInfo();
+
+            Car Panda = new Car();
+            Panda.Brand = "Fiat";
+            Panda.Model = "Panda";
+            Panda.Color = "White";
+            Panda.Price = 13000;
+            Panda.Plate = "AA000AA";
+
+            Console.WriteLine(Panda.KmPerRefill(40));
+        }
+    }
+}
--- 13_settimana/BackEnd_1/BackEnd_1/Program.cs
+++ 13_settimana/BackEnd_1/BackEnd_1/Program.cs
@@ -1,138 +1,143 @@
-﻿public class Atleta
+﻿namespace BackEnd_1
 {
-    string firstName;
-    string lastName;
-    string sport;
-    int age;
+    public class Atleta
+    {
+        string firstName;
+        string lastName;
+        string sport;
+        int age;
 
-    public string FirstName { 
-        get { return firstName; }
-        set { firstName = value; }
-    }
-    public string LastName
-    {
-        get { return lastName; }
-        set { lastName = value; }
-    }
-    public string Sport
-    {
-        get { return sport; }
-        set { sport = value; }
-    }
-    public int Age
-    {
-        get { return age; }
-        set { age = value; }
-    }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Sport
+        {
+            get { return sport; }
+            set { sport = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
 
-    public void BriefIntroduction()
-    {
-        Console.WriteLine("Mi chiamo " + firstName + " " + lastName + " , e pratico " + sport);
-    }
+        public void BriefIntroduction()
+        {
+            Console.WriteLine("Mi chiamo " + firstName + " " + lastName + " , e pratico " + sport);
+        }
 
-}
-
-public class Dipendente
-{
-    string firstName;
-    string lastName;
-    string company;
-    string job;
-    int age;
-
-    public string FirstName
-    {
-        get { return firstName; }
-        set { firstName = value; }
-    }
-    public string LastName
-    {
-        get { return lastName; }
-        set { lastName = value; }
-    }
-    public string Company
-    {
-        get { return company; }
-        set { company = value; }
-    }
-    public string Job
-    {
-        get { return job; }
-        set { job = value; }
-    }
-    public int Age
-    {
-        get { return age; }
-        set { age = value; }
-    }
-}
-
-public class Animale
-{
-    string species;
-    string name;
-    string habitat;
-
-    public string Species
-    {
-        get { return species; }
-        set { species = value; }
-    }
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-    public string Habitat
-    {
-        get { return habitat; }
-        set { habitat = value; }
     }
 
-    public void HabitatInfo()
+    public class Dipendente
     {
-        Console.WriteLine("The " + name + "lives in " + habitat);
-    }
-}
+        string firstName;
+        string lastName;
+        string company;
+        string job;
+        int age;
 
-public class Car
-{
-    string brand;
-    string model;
-    int price;
-    string plate;
-    string color;
-
-    public string Brand
-    {
-        get { return brand; }
-        set { brand = value; }
-    }
-    public string Model
-    {
-        get { return model; }
-        set { model = value; }
-    }
-    public int Price
-    {
-        get { return price; }
-        set { price = value; }
-    }
-    public string Plate
-    {
-        get { return plate; }
-        set { plate = value; }
-    }
-    public string Color
-    {
-        get { return color; }
-        set { color = value; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+        public string Job
+        {
+            get { return job; }
+            set { job = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
     }
 
-    public int KmPerRefill(int fuelLitres)
+    public class Animale
     {
-        int kmLitres = 15;
-        int totalKm = kmLitres * fuelLitres;
-        return totalKm;
+        string species;
+        string name;
+        string habitat;
+
+        public string Species
+        {
+            get { return species; }
+            set { species = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Habitat
+        {
+            get { return habitat; }
+            set { habitat = value; }
+        }
+
+        public void HabitatInfo()
+        {
+            Console.WriteLine("The " + species + " lives in " + habitat);
+        }
     }
+
+    public class Car
+    {
+        string brand;
+        string model;
+        int price;
+        string plate;
+        string color;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Plate
+        {
+            get { return plate; }
+            set { plate = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int KmPerRefill(int fuelLitres)
+        {
+            int kmLitres = 15;
+            int totalKm = kmLitres * fuelLitres;
+            return totalKm;
+        }
+    }
+
 }
