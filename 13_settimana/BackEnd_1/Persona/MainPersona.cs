341761634abdcb0f243f4cd6c83f1bcd71f4247f
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-18 14:54:16 +0200

    w13d2, completata classe persona con i vari metodi richiesti

---
+++ 13_settimana/BackEnd_1/Persona/MainPersona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaEsercizio
+{
+    internal class MainPersona
+    {
+        public static void Main(string[] args) 
+        {
+            Persona random = new Persona();
+
+            Persona marioRossi = new Persona("Mario", "Rossi", 35);
+
+            Console.Write(random.GetNome());
+            Console.Write(" ");
+            Console.Write(random.GetCognonome());
+            Console.Write(" di anni ");
+            Console.Write(random.GetEta());
+            Console.WriteLine("");
+
+            Console.WriteLine(marioRossi.GetDettaglio());
+        }
+    }
+}
---
+++ 13_settimana/BackEnd_1/Persona/Persona.cs
@@ -0,0 +1,60 @@
+﻿namespace PersonaEsercizio
+{
+    internal class Persona
+    {
+        private string nome;
+        private string cognonome;
+        private int eta;
+
+        public Persona()
+        {
+            nome = "Anonimo";
+            cognonome = "Anonimo";
+            eta = 0;
+        }
+        public Persona(string nome, string cognonome, int eta)
+        {
+            this.nome = nome;
+            this.cognonome = cognonome;
+            this.eta = eta;
+        }
+
+        public string Nome 
+        { 
+            get { return nome; }
+            set { nome = value; }
+        }
+        
+        public string Cognonome
+        {
+            get { return cognonome; }
+            set { cognonome = value; }
+        }
+
+        public int Eta 
+        { 
+            get { return eta; } 
+            set { eta = value; } 
+        }
+
+        public string GetNome() 
+        { 
+            return nome; 
+        }
+
+        public string GetCognonome()
+        {
+            return cognonome;
+        }
+
+        public int GetEta()
+        {
+            return eta;
+        }
+        public string GetDettaglio()
+        {
+            string dettaglio = "Mi chiamo " + nome + " " + cognonome + " e ho " + eta.ToString() + "anni";
+            return dettaglio;
+        }
+    }
+}
