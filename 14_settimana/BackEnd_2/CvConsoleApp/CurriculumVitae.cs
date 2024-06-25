9ab9176015d0b139cdbd17517dc1b27a1e611688
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-06-25 15:34:35 +0200

    w14d2, terminata l'app console

---
+++ 14_settimana/BackEnd_2/CvConsoleApp/CurriculumVitae.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVConsoleApp
+{
+    internal class CurriculumVitae
+    {
+        public PersonalInformations PersonalInformations { get; set; }
+        public List<Studies> Studies { get; set; }
+        public WorkExperience CurrentJob { get; set; }
+        public List<WorkExperience> WorkExperience { get; set; }
+
+        public CurriculumVitae(PersonalInformations personalInformations, List<Studies> studies, WorkExperience currentJob, List<WorkExperience> workExperience)
+        {
+            PersonalInformations = personalInformations;
+            Studies = studies;
+            CurrentJob = currentJob;
+            WorkExperience = workExperience;
+        }
+    }
+
+    class PersonalInformations
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public PersonalInformations(string firstName, string lastName, int phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+
+    class Studies
+    {
+        public string Title { get; set; }
+        public string Institute { get; set; }
+        public DateOnly StartingDate { get; set; }
+        public DateOnly EndingDate { get;set; }
+
+        public Studies (string title, string institute, DateOnly startingDate, DateOnly endingDate)
+        {
+            Title = title;
+            Institute = institute;
+            StartingDate = startingDate;
+            EndingDate = endingDate;
+        }
+    }
+
+    class WorkExperience
+    {
+        public string Company { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public string Duties { get; set; }
+
+        public WorkExperience (string company, string jobTitle, DateTime startDate, DateTime endDate, string description, string duties)
+        {
+            Company = company;
+            JobTitle = jobTitle;
+            StartDate = startDate;
+            EndDate = endDate;
+            Description = description;
+            Duties = duties;
+        }
+    }
+}
---
+++ 14_settimana/BackEnd_2/CvConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CVConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var mrInfo = new PersonalInformations("Mario", "Rossi", 051757581, "mario@rossi.com");
+        var mrStudies = new List<Studies>
+        {
+            new Studies("Bachelor of Science", "University of Example", new DateOnly(2015, 9, 1), new DateOnly(2019, 6, 30)),
+            new Studies("Master of Science", "University of Example", new DateOnly(2019, 9, 1), new DateOnly(2021, 6, 30))
+        };
+        var mrJob = new WorkExperience("Tech Company", "Software Developer", new DateTime(2021, 7, 1), DateTime.Now, "Developing software applications", "Coding, testing, and debugging");
+        var mrPastJobs = new List<WorkExperience>
+        {
+            new WorkExperience("Old Tech Company", "Junior Developer", new DateTime(2019, 7, 1), new DateTime(2021, 6, 30), "Assisting in software development", "Assisting senior developers, fixing bugs")
+        };
+        CurriculumVitae marioRossiCV = new CurriculumVitae(mrInfo, mrStudies, mrJob, mrPastJobs);
+        PrintCVDetailsOnConsole(marioRossiCV);
+    }
+
+    static void PrintCVDetailsOnConsole(CurriculumVitae cv)
+    {
+        Console.WriteLine($"CV di {cv.PersonalInformations.FirstName} {cv.PersonalInformations.LastName}");
+        Console.WriteLine();
+        Console.WriteLine("++++ INIZIO INFORMAZIONI PERSONALI ++++");
+        Console.WriteLine($"Nome : {cv.PersonalInformations.FirstName}");
+        Console.WriteLine($"Cognome : {cv.PersonalInformations.LastName}");
+        Console.WriteLine($"Numero telfeonico : {cv.PersonalInformations.PhoneNumber}");
+        Console.WriteLine($"Email : {cv.PersonalInformations.Email}");
+        Console.WriteLine("++++ FINE INFORMAZIONI PERSONALI ++++");
+        Console.WriteLine();
+        Console.WriteLine("++++ INIZIO Studi ++++");
+        foreach (Studies study in cv.Studies) 
+        {
+            Console.WriteLine($"Istituto: {study.Institute}");
+            Console.WriteLine($"Qualifica: {study.Title}");
+            Console.WriteLine($"Dal {study.StartingDate} al {study.EndingDate}");
+        }
+        Console.WriteLine("++++ FINE Studi ++++");
+        Console.WriteLine();
+        Console.WriteLine("++++ INIZIO Impiego ++++");
+        PrintWorkExp(cv.CurrentJob);
+        Console.WriteLine("++++ FINE Impiego ++++");
+        Console.WriteLine();
+        Console.WriteLine("++++ INIZIO Esperienze Lavorative ++++");
+        foreach (WorkExperience wE in cv.WorkExperience) { PrintWorkExp(wE); }
+        Console.WriteLine("++++ FINE Esperienze Lavorative ++++");
+    }
+
+    static void PrintWorkExp(WorkExperience wExp)
+    {
+        Console.WriteLine($"Presso: {wExp.Company}");
+        Console.WriteLine($"Tipo di lavoro: {wExp.JobTitle}");
+        Console.WriteLine($"Compito: {wExp.Duties}");
+        Console.WriteLine($"Dal {wExp.StartDate} al {wExp.EndDate}");
+    }
+}
